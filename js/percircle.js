761af6ff91630ee79cf2a46b729c08675f19d5ef
Chris Jones <zorlanv@gmail.com>
2015-10-05 18:14:21 +1100

    css fix for jagged edges in webkit initial load animation

--- js/percircle.js
+++ js/percircle.js
@@ -3,18 +3,29 @@ j(document).ready(function () {
 	var rotationMultiplier = 3.6;
 	// For each div that its id ends with "circle", do the following.
 	j('div.c100').each(function() {
-        var dataPercent = j(this).attr('data-percent') || 0;
-        if (dataPercent > 50) j(this).addClass("gt50");
-        var dataText = j(this).attr('data-text') || dataPercent + '%';
-        j('<span>'+dataText+'</span>').appendTo(this);
-        j('<div class="slice"><div class="bar"></div><div class="fill"></div></div>').appendTo(this);
-        var rotationDegrees = rotationMultiplier*dataPercent;
-        j('.bar', this).css({
-          '-webkit-transform' : 'rotate(' + rotationDegrees + 'deg)',
-          '-moz-transform'    : 'rotate(' + rotationDegrees + 'deg)',
-          '-ms-transform'     : 'rotate(' + rotationDegrees + 'deg)',
-          '-o-transform'      : 'rotate(' + rotationDegrees + 'deg)',
-          'transform'         : 'rotate(' + rotationDegrees + 'deg)'
+        var percircle = this;
+        var dataPercent = j(percircle).attr('data-percent') || 0;
+        if (dataPercent > 50) j(percircle).addClass("gt50");
+        var dataText = j(percircle).attr('data-text') || dataPercent + '%';
+        j('<span>'+dataText+'</span>').appendTo(percircle);
+        j('<div class="slice"><div class="bar"></div><div class="fill"></div></div>').appendTo(percircle);
+        if (dataPercent > 50)
+        j('.bar', percircle).css({
+          '-webkit-transform' : 'rotate(' + 180 + 'deg)',
+          '-moz-transform'    : 'rotate(' + 180 + 'deg)',
+          '-ms-transform'     : 'rotate(' + 180 + 'deg)',
+          '-o-transform'      : 'rotate(' + 180 + 'deg)',
+          'transform'         : 'rotate(' + 180 + 'deg)'
         });
+        var rotationDegrees = rotationMultiplier*dataPercent;
+        setTimeout(function(){
+            j('.bar', percircle).css({
+              '-webkit-transform' : 'rotate(' + rotationDegrees + 'deg)',
+              '-moz-transform'    : 'rotate(' + rotationDegrees + 'deg)',
+              '-ms-transform'     : 'rotate(' + rotationDegrees + 'deg)',
+              '-o-transform'      : 'rotate(' + rotationDegrees + 'deg)',
+              'transform'         : 'rotate(' + rotationDegrees + 'deg)'
+            });
+        }, 1);
     });
 });
