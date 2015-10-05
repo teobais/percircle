508bb9393fe67f39caf62a8096dd705542915005
Chris Jones <zorlanv@gmail.com>
2015-10-05 11:54:55 +1100

    Simplified css: replaced percentage classes with single class when percent greater than 50, replaced em adjustments with transform scale, alternate vertical centering, added red color, removed clearfix rules Updated javascript: made function act upon divs with class 'c100', added data-percent and data-text attributes for percentage and inner text respectively; slice, bar and fill divs are now added by the javascript Updated index.html with examples

--- js/percircle.js
+++ js/percircle.js
@@ -1,25 +1,20 @@
-jQuery(document).ready(function () {
+var j = jQuery.noConflict();
+j(document).ready(function () {
 	var rotationMultiplier = 3.6;
 	// For each div that its id ends with "circle", do the following.
-	jQuery( "div[id$='circle']" ).each(function() {
-		// Save all of its classes in an array.
-		var classList = jQuery( this ).attr('class').split(/\s+/);
-		// Iterate over the array
-		for (var i = 0; i < classList.length; i++) {
-		   /* If there's about a percentage class, take the actual percentage and apply the
-				css transformations in all occurences of the specified percentage class,
-				even for the divs without an id ending with "circle" */
-		   if (classList[i].match("^p")) {
-			var rotationPercentage = classList[i].substring(1, classList[i].length);
-			var rotationDegrees = rotationMultiplier*rotationPercentage;
-			jQuery('.c100.p'+rotationPercentage+ ' .bar').css({
-			  '-webkit-transform' : 'rotate(' + rotationDegrees + 'deg)',
-			  '-moz-transform'    : 'rotate(' + rotationDegrees + 'deg)',
-			  '-ms-transform'     : 'rotate(' + rotationDegrees + 'deg)',
-			  '-o-transform'      : 'rotate(' + rotationDegrees + 'deg)',
-			  'transform'         : 'rotate(' + rotationDegrees + 'deg)'
-			});
-		   }
-		}
-	});
+	j('div.c100').each(function() {
+        var dataPercent = j(this).attr('data-percent') || 0;
+        if (dataPercent > 50) j(this).addClass("gt50");
+        var dataText = j(this).attr('data-text') || dataPercent + '%';
+        j('<span>'+dataText+'</span>').appendTo(this);
+        j('<div class="slice"><div class="bar"></div><div class="fill"></div></div>').appendTo(this);
+        var rotationDegrees = rotationMultiplier*dataPercent;
+        j('.bar', this).css({
+          '-webkit-transform' : 'rotate(' + rotationDegrees + 'deg)',
+          '-moz-transform'    : 'rotate(' + rotationDegrees + 'deg)',
+          '-ms-transform'     : 'rotate(' + rotationDegrees + 'deg)',
+          '-o-transform'      : 'rotate(' + rotationDegrees + 'deg)',
+          'transform'         : 'rotate(' + rotationDegrees + 'deg)'
+        });
+    });
 });
