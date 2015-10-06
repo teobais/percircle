712eed7f2bf896541330536afd3e312108ddd1da
Chris Hardie <chrishardie@outlook.com>
2015-10-05 22:01:07 -0400

    #4 Refactored js to use data attribute

--- js/percircle.js
+++ js/percircle.js
@@ -1,25 +1,36 @@
 jQuery(document).ready(function () {
-	var rotationMultiplier = 3.6;
-	// For each div that its id ends with "circle", do the following.
-	jQuery( "div[id$='circle']" ).each(function() {
-		// Save all of its classes in an array.
-		var classList = jQuery( this ).attr('class').split(/\s+/);
-		// Iterate over the array
-		for (var i = 0; i < classList.length; i++) {
-		   /* If there's about a percentage class, take the actual percentage and apply the
-				css transformations in all occurences of the specified percentage class,
-				even for the divs without an id ending with "circle" */
-		   if (classList[i].match("^p")) {
-			var rotationPercentage = classList[i].substring(1, classList[i].length);
-			var rotationDegrees = rotationMultiplier*rotationPercentage;
-			jQuery('.c100.p'+rotationPercentage+ ' .bar').css({
-			  '-webkit-transform' : 'rotate(' + rotationDegrees + 'deg)',
-			  '-moz-transform'    : 'rotate(' + rotationDegrees + 'deg)',
-			  '-ms-transform'     : 'rotate(' + rotationDegrees + 'deg)',
-			  '-o-transform'      : 'rotate(' + rotationDegrees + 'deg)',
-			  'transform'         : 'rotate(' + rotationDegrees + 'deg)'
-			});
-		   }
+
+	//Identify by data attribute
+  var dataPercircles = jQuery( "div[data-percircle]" );
+	//Identify by id, ensuring we don't percircle divs identified above
+	var idPercircles = jQuery( "div[id$='circle']" ).not( "[data-percircle]" );
+
+  jQuery.each( dataPercircles, function(){
+		var percentage = $(this).data( "percircle" );
+		render( this, percentage )
+	});
+
+	jQuery.each( idPercircles, function(){
+		var classes = this.className.split( /\s+/ );
+		var percentageClass = classes.filter(function( value ){
+			return value.match( /^p\d+/ );
+		});
+
+		if( percentageClass.length > 0 ){
+			var percentage = percentageClass[0].substring(1);
+			render( this, percentage );
 		}
 	});
+
+	function render( node, percentage ){
+		var rotationMultiplier = 3.6;
+		var rotationDegrees = rotationMultiplier * percentage;
+		jQuery( node ).find( '.bar' ).css({
+			'-webkit-transform' : 'rotate(' + rotationDegrees + 'deg)',
+			'-moz-transform'    : 'rotate(' + rotationDegrees + 'deg)',
+			'-ms-transform'     : 'rotate(' + rotationDegrees + 'deg)',
+			'-o-transform'      : 'rotate(' + rotationDegrees + 'deg)',
+			'transform'         : 'rotate(' + rotationDegrees + 'deg)'
+		});
+	}
 });
