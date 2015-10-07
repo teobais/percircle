89eadca62af5ddbfc944cc22f5756545d3bc0623
Chris Hardie <chrishardie@outlook.com>
2015-10-06 20:12:09 -0400

    #4 Ensured proper class was set on percircles, prevented ids from being used to identify targets.

--- js/percircle.js
+++ js/percircle.js
@@ -2,24 +2,11 @@ jQuery(document).ready(function () {
 
 	//Identify by data attribute
 	var dataPercircles = jQuery( "div[data-percircle]" );
-	//Identify by id, ensuring we don't percircle divs identified above
-	var idPercircles = jQuery( "div[id$='circle']" ).not( "[data-percircle]" );
 
 	jQuery.each( dataPercircles, function(){
-		var percentage = $(this).data( "percircle" );
+		var percentage = jQuery( this ).data( "percircle" );
+    jQuery( this ).addClass( 'p' + percentage);
 		render( this, percentage )
-	});
-
-	jQuery.each( idPercircles, function(){
-		var classes = this.className.split( /\s+/ );
-		var percentageClass = classes.filter(function( value ){
-			return value.match( /^p\d+/ );
-		});
-
-		if( percentageClass.length > 0 ){
-			var percentage = percentageClass[0].substring(1);
-			render( this, percentage );
-		}
 	});
 
 	function render( node, percentage ){
